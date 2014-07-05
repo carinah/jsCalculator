4e766feb81a0c6381c53dc475e689d6b3a04662c
carinah <cyingho@gmail.com>
2014-07-05 11:24:45 -0700

    Working calculator

--- calculator.js
+++ calculator.js
@@ -1,79 +1,95 @@
-// displays numbers on screen and performs math operations 
-function calc(val) {
+var val1 = false;
+var val2 = false;
+var operator = false;
+
+var evaluate = function(val1, operator, val2) {
+
+  switch (operator) {
+    case "+":
+      var result = val1 + val2;
+      document.getElementById("screen").value=result;
+      break;
+
+    case "-":
+      var result = val1 - val2;
+      document.getElementById("screen").value=result;
+      break;
+
+    case "/":
+      var result = val1/val2;
+      document.getElementById("screen").value=result;
+      break;
+
+    case "*":
+      var result = val1*val2;
+      document.getElementById("screen").value=result;
+      break;
+    default:
+      console.log("No matches found!!");
+      break;
+  }
+};
+
+function calc(event) {
+  var val = event.target.value;
   document.getElementById("screen").value=val;
 
-// create variables that store vals. 
-  var val1 = false;
-  var val2 = false;
-  var operator = false;
-  var math_symbols = ['+', '-', '/', '*']; 
-  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// evaluate if inputs are numbers or operators
+// isNumber function determines if event is a #. 
+  var isNumber = function(event) { 
+    return !isNaN(Number(event)); 
+  };
 
-  function doMath(val) {
-      for (var count=0; count < numbers.length; count++) {
-        if (val == numbers[count]) {
-          val1 = val;
-        } else { 
-          count++; 
-        }
-        val1
+// isOperator function determines if event is an operator. 
+  function isOperator(val) {
+    var math_symbols = ['+', '-', '/', '*']; 
+    for (var i=0; i < math_symbols.length; i++) { 
+      if (val === math_symbols[i]) {
+        return true;
       }
+    } 
+    return false;
+  };
 
-      for (var i=0; i < math_symbols.length; i++) { 
-        if (val == math_symbols[i] && val1!=false) {
-          operator = math_symbols[i];
-        } else {
-          i++
-        }
-        return operator;
-      }
 
-      for (var count=0; count < numbers.length; count++) {
-        if (val1!=false && operator!=false && val == numbers[count]) {
-          val2 = parseInt(val);
-        } else {
-          count++;
-        }
-        return val2;
-        }
+  if (isNumber(val)===true && val1===false) {
+    val1= val;
+  } else if (isOperator(val)===true && val1!==false) {
+    operator=val;
+  } else if (isNumber(val)===true && val1!==false && operator!==false) {
+    val2=val;
+  } else if (val==="=" && val1!==false && val2!==false && operator!==false) {
+    evaluate(val1, operator, val2);
   }
-
-// Evaluate operators to do math. 
-
-switch (operator) {
-  case operator === "+":
-    var result = val1 + val2;
-    document.getElementById("screen").value=result;
-    break;
-
-  case operator === "-":
-    var result = val1 - val2;
-    document.getElementById("screen").value=result;
-    break;
-
-    case operator === "/":
-    var result = val1/val2;
-    document.getElementById("screen").value=result;
-    break;
-
-    case operator === "*":
-    var result = val1*val2;
-    document.getElementById("screen").value=result;
-    break;
-
-    }
 };
-
-
-function e() {
-  document.getElementById("screen").value="";  
-};
-
 
 // clear value from screen
-function clr(val) { 
-  document.getElementById("screen").value=val;
+function clr(event) { 
+  val1=false;
+  val2=false;
+  operator=false;
+  document.getElementById("screen").value="";
 };
+
+
+document.getElementById('button_1').addEventListener("click", calc );
+document.getElementById('button_2').addEventListener("click", calc );
+document.getElementById('button_3').addEventListener("click", calc );
+document.getElementById('button_4').addEventListener("click", calc );
+document.getElementById('button_5').addEventListener("click", calc );
+document.getElementById('button_6').addEventListener("click", calc );
+document.getElementById('button_7').addEventListener("click", calc );
+document.getElementById('button_8').addEventListener("click", calc );
+document.getElementById('button_9').addEventListener("click", calc );
+document.getElementById('button_0').addEventListener("click", calc );
+
+
+document.getElementById('button_fwdslash').addEventListener("click", calc );
+document.getElementById('button_asterisk').addEventListener("click", calc );
+document.getElementById('button_minus').addEventListener("click", calc );
+document.getElementById('button_plus').addEventListener("click", calc );
+document.getElementById('button_equal').addEventListener("click", calc );
+document.getElementById('button_period').addEventListener("click", calc );
+
+document.getElementById('button_clear').addEventListener("click", clr );
 
