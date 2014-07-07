c784239b9245add95aa0957e0e564b714068e2bc
carinah <cyingho@gmail.com>
2014-07-06 22:04:18 -0700

    modified calc method to display more than single digit input

--- calculator.js
+++ calculator.js
@@ -6,17 +6,17 @@ var evaluate = function(val1, operator, val2) {
 
   switch (operator) {
     case "+":
-      var result = val1 + val2;
+      var result = Number(val1 + val2);
       document.getElementById("screen").value=result;
       break;
 
     case "-":
-      var result = val1 - val2;
+      var result = Number(val1 - val2);
       document.getElementById("screen").value=result;
       break;
 
     case "/":
-      var result = val1/val2;
+      var result = Number(val1/val2);
       document.getElementById("screen").value=result;
       break;
 
@@ -24,24 +24,19 @@ var evaluate = function(val1, operator, val2) {
       var result = val1*val2;
       document.getElementById("screen").value=result;
       break;
-    default:
-      console.log("No matches found!!");
-      break;
   }
 };
 
 function calc(event) {
   var val = event.target.value;
-  document.getElementById("screen").value=val;
-
-
+  
 // isNumber function determines if event is a #. 
   var isNumber = function(event) { 
     return !isNaN(Number(event)); 
   };
 
 // isOperator function determines if event is an operator. 
-  function isOperator(val) {
+  var isOperator = function(val) {
     var math_symbols = ['+', '-', '/', '*']; 
     for (var i=0; i < math_symbols.length; i++) { 
       if (val === math_symbols[i]) {
@@ -51,24 +46,39 @@ function calc(event) {
     return false;
   };
 
+/* 
+- if val is a # and val is false, val1 = val 
+- if val is a # and val is true and operator is false, val1 + val 
+- if val is an operator and operator is val1 is not false 
+-  if val is a number and val1 is not false and operator is not false, val2 = val
+- if val is a number and val1 is not false and operator is not false and val2 is not false
+*/ 
 
   if (isNumber(val)===true && val1===false) {
     val1= val;
+    document.getElementById("screen").value=val1;
+  } else if (isNumber(val)===true && val1!==false && operator===false) {
+    val1 = Number(val1 + val);
+    document.getElementById("screen").value=val1;
   } else if (isOperator(val)===true && val1!==false) {
     operator=val;
-  } else if (isNumber(val)===true && val1!==false && operator!==false) {
-    val2=val;
+  } else if (isNumber(val)===true && val1!==false && operator!==false && val2===false) {
+    val2=Number(val);
+    document.getElementById("screen").value=val2;
+  } else if (isNumber(val)===true && val1!==false && val2!==false && operator!==false) {
+    val2 = Number(val2 + val);
+    document.getElementById("screen").value=val2;
   } else if (val==="=" && val1!==false && val2!==false && operator!==false) {
     evaluate(val1, operator, val2);
   }
-};
 
+}
 // clear value from screen
 function clr(event) { 
   val1=false;
   val2=false;
   operator=false;
-  document.getElementById("screen").value="";
+  document.getElementById("screen").value="0";
 };
 
 
